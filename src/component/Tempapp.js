0188ca223a57c91ce0bbe427b7a4b32b476297b6
aneeshyder <anishcharoliya@gmail.com>
2021-04-28 17:02:07 +0530

    first commit

---
+++ src/component/Tempapp.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import "./css/style.css";
+
+const Tempapp = () => {
+    const [city, setCity] = useState(null);
+    const [sysinfo, setSysinfo] = useState(null);
+    const [weainfo, setWeainfo] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
+    useEffect ( () => {               
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=c5dac860e4358adefaac387c2ed817ff            `;
+            const response = await fetch(url);            
+            const resJson = await response.json();
+            setCity(resJson.main);
+            setSysinfo(resJson.sys);
+            setWeainfo(resJson.weather);            
+        }
+
+        fetchApi();
+    },[search])
+
+    return (
+        <>
+        <h1>Weather App</h1>
+            <div className="box">
+                <div className="inputData">
+                    <input
+                     type="search"
+                     value={search}
+                    className="inputFeild"
+                    onChange = { (event) => {setSearch(event.target.value)}}
+                    />
+                </div>
+            </div>
+            {!city || !sysinfo || !weainfo ? (
+                <p className="no-data"> No data found</p>
+            ) : (
+                <>
+                <div className="info">
+                    <h2 className="location"><i class="fas fa-map-marker-alt"></i> {search}, {sysinfo.country}</h2>                    
+                    <div className="weather-img"><img src={"http://openweathermap.org/img/wn/" + weainfo[0].icon + "@2x.png"} /></div>
+                    <p className="weather-name">{weainfo[0].description}</p>
+                    
+                    <h1 className="temp">{city.temp}°cel</h1>
+                    <p className="temp_min_max"><i class="fa fa-arrow-down"></i> {city.temp_min}°cel <i class="fa fa-arrow-up"></i> {city.temp_max}°cel</p>
+
+                </div>
+                </>
+            )
+
+            }
+            
+        </>
+    )
+}
+
+export default Tempapp;
